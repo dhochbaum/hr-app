5a86ea4a76dfd026a462b3cdde7a526946c75880
dhochbaum <dhochbaum@gmail.com>
2019-12-05 19:31:19 -0500

    Duplicated create user form in AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,6 +6,12 @@ namespace App\Controller;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
 use App\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Require ROLE_ADMIN for *every* controller method in this class.
@@ -44,5 +50,44 @@ class AdminController extends EasyAdminController
         return $encoder->encodePassword($password, $user->getSalt());
     }
 
+    /**
+     * @Route("/create-user", name="create_user")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        // if ($form->isSubmitted()) {
+
+        //     exit(\Doctrine\Common\Util\Debug::dump($user));
+
+        // }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            // exit(\Doctrine\Common\Util\Debug::dump($user));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('easyadmin');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
 
 }
